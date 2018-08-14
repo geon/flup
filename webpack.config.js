ed9761607f3f46588caf14f2cbd1717b912cd184
Victor Widell <victor.widell@divid.se>
2018-08-14 12:34:15 +0200

    Fixed build scripts.

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/dist/"
+    publicPath: "/"
   },
   module: {
     rules: [
