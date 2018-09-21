cf4a4815aa7b053211ade95f3dc437381e4c3d3b
Victor Widell <victor.widell@divid.se>
2018-09-22 00:54:04 +0200

    Implemented stupid AI opponent.

--- src/GameMode2pAi.ts
+++ src/GameMode2pAi.ts
@@ -7,9 +7,67 @@ import { Coord } from "./Coord";
 import { GameMode } from "./GameMode";
 import { PieceCycle } from "./PieceCycle";
 import { AvatarMonolith } from "./AvatarMonolith";
+import { waitMs } from "./Animation";
+import { Dropper } from "./Dropper";
 
 function randomArrayElement<T>(array: ReadonlyArray<T>): T {
 	return array[Math.floor(Math.random() * array.length)];
+}
+
+interface DropperPose {
+	position: number;
+	orientation: "horizontal" | "vertical";
+}
+
+function findMatchingDrop(
+	dropper: Dropper,
+): { matchingPose: DropperPose; ascending: boolean } {
+	// This table has neighbours of allnumber, number]possible combinations, and still fits within the width of the board.
+	const colorTable = [0, 1, 2, 3, 1, 3, 0, 2];
+
+	const dropColors: [number, number] = [
+		dropper.pieceA.color,
+		dropper.pieceB.color,
+	];
+
+	if (dropColors[0] === dropColors[1]) {
+		for (let position = 0; position < colorTable.length; ++position) {
+			if (colorTable[position] === dropColors[0]) {
+				return {
+					matchingPose: { position, orientation: "vertical" },
+					ascending: false,
+				};
+			}
+		}
+	}
+
+	for (let position = 0; position < colorTable.length - 1; ++position) {
+		if (
+			colorTable[position] === dropColors[0] &&
+			colorTable[position + 1] === dropColors[1]
+		) {
+			return {
+				matchingPose: { position, orientation: "horizontal" },
+				ascending: dropColors[0] < dropColors[1],
+			};
+		}
+	}
+
+	dropColors.reverse();
+
+	let position = 0;
+	for (; position < colorTable.length - 1; ++position) {
+		if (
+			colorTable[position] === dropColors[0] &&
+			colorTable[position + 1] === dropColors[1]
+		) {
+			break;
+		}
+	}
+	return {
+		matchingPose: { position, orientation: "horizontal" },
+		ascending: dropColors[0] < dropColors[1],
+	};
 }
 
 export class GameMode2pAi implements GameMode {
@@ -101,9 +159,13 @@ export class GameMode2pAi implements GameMode {
 	}
 
 	*makeFrameCoroutine(): IterableIterator<void> {
+		const aiCoroutine = this.makeAiCoroutine();
+
 		// Run board coroutines concurrently.
 		for (;;) {
 			const deltaTime: number = yield;
+
+			aiCoroutine.next(deltaTime);
 
 			this.boards
 				.map(board => board.frameCoroutine)
@@ -117,6 +179,67 @@ export class GameMode2pAi implements GameMode {
 			// 	break;
 			// }
 		}
+	}
+
+	*makeAiCoroutine(): IterableIterator<void> {
+		for (;;) {
+			// Simulate thinking time.
+			yield* waitMs(500);
+
+			const dropper = this.boards[0].boardLogic.dropper;
+			const { matchingPose, ascending } = findMatchingDrop(dropper);
+
+			let move: (() => void) | undefined;
+			for (;;) {
+				move = this.nextMoveToGetToPose(
+					{
+						position: dropper.position,
+						orientation: dropper.orientation,
+					},
+					matchingPose,
+					ascending,
+				);
+
+				if (!move) {
+					break;
+				}
+
+				// Simulate slow fingers.
+				yield* waitMs(150);
+
+				move();
+			}
+
+			// Simulate hesitation.
+			yield* waitMs(200);
+			this.boards[0].drop();
+		}
+	}
+
+	nextMoveToGetToPose(
+		from: DropperPose,
+		to: DropperPose,
+		ascending: boolean,
+	): (() => void) | undefined {
+		const dropper = this.boards[0].boardLogic.dropper;
+		const dropColors: [number, number] = [
+			dropper.pieceA.color,
+			dropper.pieceB.color,
+		];
+
+		if (from.orientation != to.orientation) {
+			return () => this.boards[0].rotate();
+		}
+		if (ascending !== dropColors[0] < dropColors[1]) {
+			return () => this.boards[0].rotate();
+		}
+		if (from.position < to.position) {
+			return () => this.boards[0].moveRight();
+		}
+		if (from.position > to.position) {
+			return () => this.boards[0].moveLeft();
+		}
+		return undefined;
 	}
 
 	draw(context: CanvasRenderingContext2D, appSize: Coord) {
